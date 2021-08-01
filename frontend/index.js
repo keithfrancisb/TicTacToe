60dc5b93e5bf31a6148378d22312cc8bcb7ea07a
Keith Francis Balde <keithfrancisb@gmail.com>
2021-08-01 12:28:48 -0400

    add draw modal

--- frontend/index.js
+++ frontend/index.js
@@ -14,6 +14,8 @@ const player2 = '0';
 let isPlayer1Turn = true;
 let currentTurn = isPlayer1Turn ? player1 : player2;
 
+let i=0;
+
 // Add functionality to each square to mark player's Xs or Os
 const squares = document.getElementsByClassName('symbol');
 
@@ -27,11 +29,35 @@ const squaresArray = Array.from(squares);
 //   });
 // }
 
+const changeTurn = () => {
+  isPlayer1Turn = !isPlayer1Turn;
+  currentTurn = isPlayer1Turn ? player1 : player2;
+};
+
+const checkForDraw = () => {
+  i++;
+
+  if (i==9) {
+    // show draw result
+    const modal = document.getElementById('modal');
+
+    // grab win result element
+    // set innerHTML
+    const winResult = document.getElementById('win-result');
+    
+    modal.classList.add('game-over');
+    winResult.innerHTML = 'Draw';
+  }
+};
+
+
 const setupSquare = (square) => {
   square.addEventListener('click', () => {
     square.innerHTML = currentTurn;
-    isPlayer1Turn = !isPlayer1Turn;
-    currentTurn = isPlayer1Turn ? player1 : player2;
+
+    changeTurn();
+
+    checkForDraw();
   });
 }
 
