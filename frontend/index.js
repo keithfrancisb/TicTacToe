971a73bf0e0f012fd38849f929f61674422a1773
Keith Francis Balde <keithfrancisb@gmail.com>
2021-08-01 11:27:50 -0400

    setup alternating turns Xs and Os

--- frontend/index.js
+++ frontend/index.js
@@ -8,23 +8,31 @@
  * Show Winner result
  */
 
-const board = document.getElementById('board');
+const player1 = 'X';
+const player2 = '0';
+
+let isPlayer1Turn = true;
+let currentTurn = isPlayer1Turn ? player1 : player2;
 
 // Add functionality to each square to mark player's Xs or Os
 const squares = document.getElementsByClassName('symbol');
 
 const squaresArray = Array.from(squares);
 
-// for(let i=0; i < squaresArray.length; i++) {
-//   const square = squaresArray[i];
+// for(let i=0; i < squares.length; i++) {
+//   const square = squares[i];
 
 //   square.addEventListener('click', () => {
 //     square.innerHTML = 'X';
 //   });
 // }
 
-squaresArray.forEach((square) => {
+const setupSquare = (square) => {
   square.addEventListener('click', () => {
-    square.innerHTML = 'X';
+    square.innerHTML = currentTurn;
+    isPlayer1Turn = !isPlayer1Turn;
+    currentTurn = isPlayer1Turn ? player1 : player2;
   });
-});
+}
+
+squaresArray.forEach(setupSquare);
