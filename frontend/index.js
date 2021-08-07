026fd8693febf3090ee1c0187fb9840c73876ea3
Keith Francis Balde <keithfrancisb@gmail.com>
2021-08-07 09:21:41 -0400

    add new changes from Zek

--- frontend/index.js
+++ frontend/index.js
@@ -8,62 +8,65 @@
  * Show Winner result
  */
 
-const player1 = 'X';
-const player2 = '0';
+ const player1 = 'X';
+ const player2 = '0';
+ 
+ let isPlayer1Turn = true;
+ let currentTurn = isPlayer1Turn ? player1 : player2;
+ 
+ let i=0;
+ let j=0;
+ 
+ // Add functionality to each square to mark player's Xs or Os
+ const squares = document.getElementsByClassName('symbol');
+ 
+ const squaresArray = Array.from(squares); 
+ 
+ const changeTurn = () => {
+   isPlayer1Turn = !isPlayer1Turn;
+   currentTurn = isPlayer1Turn ? player1 : player2;
+ };
 
-let isPlayer1Turn = true;
-let currentTurn = isPlayer1Turn ? player1 : player2;
+ const checkForDraw = () => {
+   i++;
+   if (i==9) {
+     // show draw result
+     const modal = document.getElementById('modal');
+ 
+     // grab win result element
+     // set innerHTML
+     const winResult = document.getElementById('win-result');
+     
+     modal.classList.add('game-over');
+     winResult.innerHTML = 'Draw';
+    i=i-9;
 
-let i=0;
+   }
+ };
+ const restart = document.getElementById('sub_btn');
 
-// Add functionality to each square to mark player's Xs or Os
-const squares = document.getElementsByClassName('symbol');
+const startgame = (square) =>{
+  modal.classList.remove('game-over');
+  squares.getElementsByClassName('symbol').innerHTML = "";
 
-const squaresArray = Array.from(squares);
-
-// for(let i=0; i < squares.length; i++) {
-//   const square = squares[i];
-
-//   square.addEventListener('click', () => {
-//     square.innerHTML = 'X';
-//   });
-// }
-
-const changeTurn = () => {
-  isPlayer1Turn = !isPlayer1Turn;
-  currentTurn = isPlayer1Turn ? player1 : player2;
-};
-
-const checkForDraw = () => {
-  i++;
-
-  if (i==9) {
-    // show draw result
-    const modal = document.getElementById('modal');
-
-    // grab win result element
-    // set innerHTML
-    const winResult = document.getElementById('win-result');
-    
-    modal.classList.add('game-over');
-    winResult.innerHTML = 'Draw';
-  }
-};
-
-
-const setupSquare = (square) => {
-  square.addEventListener('click', () => {
-    square.innerHTML = currentTurn;
-
-    changeTurn();
-
-    checkForDraw();
-  });
 }
+sub_btn.addEventListener('click',startgame);
+ 
+ const setupSquare = (square) => {
+   square.addEventListener('click', () => {
+     square.innerHTML = currentTurn;
+ 
+     changeTurn();
+ 
+     checkForDraw();
+     j=1;
+   });
+ }
 
-squaresArray.forEach(setupSquare);
-
-/**
- * Make html with html elements
- * use js to play with them
- */
+ 
+ squaresArray.forEach(setupSquare);
+ 
+ /**
+  * Make html with html elements
+  * use js to play with them
+  */
