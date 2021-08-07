4dff9792290ade9b24a9467963fb50e940489b86
Keith Francis Balde <keithfrancisb@gmail.com>
2021-08-07 10:14:16 -0400

    setup restart button

--- frontend/index.js
+++ frontend/index.js
@@ -8,63 +8,60 @@
  * Show Winner result
  */
 
- const player1 = 'X';
- const player2 = '0';
- 
- let isPlayer1Turn = true;
- let currentTurn = isPlayer1Turn ? player1 : player2;
- 
- let i=0;
- let j=0;
- 
- // Add functionality to each square to mark player's Xs or Os
- const squares = document.getElementsByClassName('symbol');
- 
- const squaresArray = Array.from(squares); 
- 
- const changeTurn = () => {
-   isPlayer1Turn = !isPlayer1Turn;
-   currentTurn = isPlayer1Turn ? player1 : player2;
- };
+const player1 = 'X';
+const player2 = '0';
 
- const checkForDraw = () => {
-   i++;
-   if (i==9) {
-     // show draw result
-     const modal = document.getElementById('modal');
- 
-     // grab win result element
-     // set innerHTML
-     const winResult = document.getElementById('win-result');
-     
-     modal.classList.add('game-over');
-     winResult.innerHTML = 'Draw';
-    i=i-9;
+let isPlayer1Turn = true;
+let currentTurn = isPlayer1Turn ? player1 : player2;
 
-   }
- };
- const restart = document.getElementById('sub_btn');
+let i=0;
+// Add functionality to each square to mark player's Xs or Os
+const squares = document.getElementsByClassName('symbol');
 
-const startgame = (square) =>{
+const squaresArray = Array.from(squares);
+
+const changeTurn = () => {
+  isPlayer1Turn = !isPlayer1Turn;
+  currentTurn = isPlayer1Turn ? player1 : player2;
+};
+
+const checkForDraw = () => {
+  i++;
+  if (i==9) {
+    // show draw result
+    const modal = document.getElementById('modal');
+
+    // grab win result element
+    // set innerHTML
+    const winResult = document.getElementById('win-result');
+    
+    modal.classList.add('game-over');
+    winResult.innerHTML = 'Draw';
+
+    i=0;
+  }
+};
+
+const restartButton = document.getElementById('restart-button');
+
+const startgame = () => {
   modal.classList.remove('game-over');
-  squares.getElementsByClassName('symbol').innerHTML = "";
+  squaresArray.forEach(input => input.innerHTML ='');
+};
 
-}
-sub_btn.addEventListener('click',startgame);
+restartButton.addEventListener('click', startgame);
  
- const setupSquare = (square) => {
-   square.addEventListener('click', () => {
-     square.innerHTML = currentTurn;
- 
-     changeTurn();
- 
-     checkForDraw();
-     j=1;
-   });
- }
+const setupSquare = (square) => {
+  square.addEventListener('click', () => {
+    square.innerHTML = currentTurn;
 
+    changeTurn();
+
+    checkForDraw();
+  });
+};
  
- squaresArray.forEach(setupSquare);
+squaresArray.forEach(setupSquare);
  
  /**
   * Make html with html elements
